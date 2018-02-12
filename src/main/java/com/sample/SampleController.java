bec11a9dce49af02a89bf0f552df227edd84594d
erankit23 <ankit230790@gmail.com>
2018-02-12 23:35:56 +0530

    Update SampleController.java

--- src/main/java/com/sample/SampleController.java
+++ src/main/java/com/sample/SampleController.java
@@ -33,7 +33,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World! I am Ankit";
     }
     
     @RequestMapping("/welcome")
